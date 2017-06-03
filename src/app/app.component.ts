552287a076ef452f9a85e475d29482e51fd52949
Hifumisama <m.khayat92@gmail.com>
2017-06-03 18:47:26 +0200

    1 er chapitre terminé, cela consiste à importer un module, modifier un template et jouer avec le data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>{{title}}</h1>
+  <h2>Vous vous nommez {{hero.name}}</h2>
+  <label>Votre nom est ?</label>
+  <input [(ngModel)]="hero.name" placeholder="name">`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { title = "League des Héros"; hero: Hero = {
+  id: 1,
+  name: "Superman",
+}    }
+
+export class Hero {
+  id: number;
+  name: string;
+}
