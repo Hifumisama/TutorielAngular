622ca465235a8e300154e76425e70b44f56e9534
Hifumisama <m.khayat92@gmail.com>
2017-06-03 19:55:18 +0200

    2 e chapitre terminé, on a ici découvert les directives ng..., on a ajouté un évènement dans un template, récupéré une valeur provenant de l'input utilisateur puis réaffiché celle ci dans un formulaire

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -6,27 +6,53 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
-var AppComponent = (function () {
-    function AppComponent() {
-        this.title = "League des Héros";
-        this.hero = {
-            id: 1,
-            name: "Superman",
-        };
-    }
-    return AppComponent;
-}());
-AppComponent = __decorate([
-    core_1.Component({
-        selector: 'my-app',
-        template: "<h1>{{title}}</h1>\n  <h2>Vous vous nommez {{hero.name}}</h2>\n  <label>Votre nom est ?</label>\n  <input [(ngModel)]=\"hero.name\" placeholder=\"name\">",
-    })
-], AppComponent);
-exports.AppComponent = AppComponent;
+// ici on a créé une classe permettant de stocker les noms
+//et Ids des héros
 var Hero = (function () {
     function Hero() {
     }
     return Hero;
 }());
 exports.Hero = Hero;
+// là on a créé un tableau contenant 10 instances de la classe
+// Hero contenant nos données
+//
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+// le décorateur "component", permet de définir où notre template
+//sera ajouté, le template associé, mais aussi éventuellement
+// la feuille de style associée
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = "League des Héros";
+        // On a affecté la variable heroes au Tableau de données heroes
+        // ici on affecte notre tableau à un des attributs de la classe, afin de la
+        // rendre appelable dans le template
+        this.heroes = HEROES;
+    }
+    // on crée une fonction qui sera appelée ensuite dans le template
+    //du composant. on affecte sa variable d'entrée à une instance de Hero
+    AppComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        templateUrl: "app/app.component.html",
+        styleUrls: ["app/app.component.css"],
+    })
+], AppComponent);
+exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,56 @@
 import { Component } from '@angular/core';
 
-
-
-@Component({
-  selector: 'my-app',
-  template: `<h1>{{title}}</h1>
-  <h2>Vous vous nommez {{hero.name}}</h2>
-  <label>Votre nom est ?</label>
-  <input [(ngModel)]="hero.name" placeholder="name">`,
-})
-export class AppComponent  { title = "League des Héros"; hero: Hero = {
-  id: 1,
-  name: "Superman",
-}    }
+// ici on a créé une classe permettant de stocker les noms
+//et Ids des héros
 
 export class Hero {
   id: number;
   name: string;
+}
+
+// là on a créé un tableau contenant 10 instances de la classe
+// Hero contenant nos données
+//
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+// le décorateur "component", permet de définir où notre template
+//sera ajouté, le template associé, mais aussi éventuellement
+// la feuille de style associée
+@Component({
+  selector: 'my-app',
+  templateUrl: "app/app.component.html",
+  styleUrls: ["app/app.component.css"],
+})
+
+// app component, cette classe correspond en gros au main de l'application,
+// Quand elle est chargée, les éléments à l'intérieur sont accessibles
+export class AppComponent {
+  title = "League des Héros";
+
+  // On a affecté la variable heroes au Tableau de données heroes
+  // ici on affecte notre tableau à un des attributs de la classe, afin de la
+  // rendre appelable dans le template
+  heroes = HEROES;
+
+  // Ici on a besoin d'initialiser la variable Selectedhero afin de récupérer
+  //1 et 1 seul héro provenant de l'intéraction de l'utilsateur
+  // Ducoup on sait aussi que la donnée sera de type hero
+  selectedHero : Hero;
+
+  // on crée une fonction qui sera appelée ensuite dans le template
+  //du composant. on affecte sa variable d'entrée à une instance de Hero
+  onSelect(hero:Hero): void {
+    this.selectedHero = hero;
+  }
 }
